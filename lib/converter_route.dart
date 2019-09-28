1ca7e72308010a6f41bc34b1be9f4969494cd752
Gyenh <blazeog@hotmail.com>
2019-09-28 20:30:15 +0200

    my first app with flutter

---
+++ lib/converter_route.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import 'unit.dart';
+
+class ConverterRoute extends StatefulWidget {
+  final Color color;
+
+  final List<Unit> units;
+
+  const ConverterRoute({
+    @required this.color,
+    @required this.units,
+  })  : assert(color != null),
+        assert(units != null);
+
+  @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  @override
+  Widget build(BuildContext context) {
+    final unitWidgets = widget.units.map((Unit unit) {
+      return Container(
+        color: widget.color,
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              unit.name,
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Text(
+              'Conversion: ${unit.conversion}',
+              style: Theme.of(context).textTheme.subhead,
+            ),
+          ],
+        ),
+      );
+    }).toList();
+
+    return ListView(
+      children: unitWidgets,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'category_route.dart';
+
+/// The function that is called when main.dart is run.
+void main() {
+  runApp(UnitConverterApp());
+}
+
+/// This widget is the root of our application.
+///
+/// The first screen we see is a list [Categories].
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: CategoryRoute(),
+    );
+  }
+}
